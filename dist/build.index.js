e6e93e6362426d21d3f3f9c9966dbc35b9d06f16
aristov <vv.aristov@gmail.com>
2017-01-22 03:57:41 +0300

    schedule

--- dist/build.index.js
+++ dist/build.index.js
@@ -3156,9 +3156,25 @@ function columnheader(init) {
     return new ColumnHeader('th', init)
 }
 
-const rows = Array.from(new Array(24));
-const ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const cells = ABC.split('');
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+
+const date = new Date;
+const daystart = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+const start = new Date(daystart.getTime() + 9 * HOUR);
+const duration = 15 * HOUR;
+const step = HOUR / 2;
+const timerows = Array.from(new Array(duration / step)).map((_, i) => {
+    return new Date(start.getTime() + i * step)
+});
+
+const ABC = 'Admin Neo Forte Piano Sol Fa p1 p2';
+const roomcolumns = ABC.split(' ');
+
+function timeformat(h, m) {
+    return [h < 10? '0' + h : h, m < 10? '0' + m : m].join(':')
+}
 
 const testgrid = grid({
     multiselectable : true,
@@ -3166,18 +3182,20 @@ const testgrid = grid({
         rowgroup({
             tagName : 'thead',
             children : row([
-                columnheader(),
-                cells.map(c => columnheader(c))
+                columnheader(date.getDate() + '/' + (date.getMonth() + 1)),
+                roomcolumns.map(c => columnheader(c))
             ])
         }),
-        rowgroup(rows.map((r, j) => row([
-            rowheader(String(j)),
-            cells.map((c, i) => gridcell({
-                disabled : i === 5 && j === 5,
-                selected : false,
-                style : { width : 95 / cells.length + '%' }
-            }))
-        ])))
+        rowgroup(timerows.map((r, j) => row({
+            dataset : { time : r.getTime() },
+            children : [
+                rowheader(timeformat(r.getHours(), r.getMinutes())),
+                roomcolumns.map(() => gridcell({
+                    selected : false,
+                    style : { width : 95 / roomcolumns.length + '%' }
+                }))
+            ]
+        })))
     ]
 });
 
--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,24 @@
-import { grid, rowgroup, row, rowheader, columnheader, gridcell } from '../lib/index'
+import { columnheader, grid, gridcell, row, rowgroup, rowheader } from '../lib/index'
 
-const rows = Array.from(new Array(24))
-const ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const cells = ABC.split('')
+const SECOND = 1000
+const MINUTE = 60 * SECOND
+const HOUR = 60 * MINUTE
+
+const date = new Date
+const daystart = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+const start = new Date(daystart.getTime() + 9 * HOUR)
+const duration = 15 * HOUR
+const step = HOUR / 2
+const timerows = Array.from(new Array(duration / step)).map((_, i) => {
+    return new Date(start.getTime() + i * step)
+})
+
+const ABC = 'Admin Neo Forte Piano Sol Fa p1 p2'
+const roomcolumns = ABC.split(' ')
+
+function timeformat(h, m) {
+    return [h < 10? '0' + h : h, m < 10? '0' + m : m].join(':')
+}
 
 const testgrid = grid({
     multiselectable : true,
@@ -10,18 +26,20 @@ const testgrid = grid({
         rowgroup({
             tagName : 'thead',
             children : row([
-                columnheader(),
-                cells.map(c => columnheader(c))
+                columnheader(date.getDate() + '/' + (date.getMonth() + 1)),
+                roomcolumns.map(c => columnheader(c))
             ])
         }),
-        rowgroup(rows.map((r, j) => row([
-            rowheader(String(j)),
-            cells.map((c, i) => gridcell({
-                disabled : i === 5 && j === 5,
-                selected : false,
-                style : { width : 95 / cells.length + '%' }
-            }))
-        ])))
+        rowgroup(timerows.map((r, j) => row({
+            dataset : { time : r.getTime() },
+            children : [
+                rowheader(timeformat(r.getHours(), r.getMinutes())),
+                roomcolumns.map(() => gridcell({
+                    selected : false,
+                    style : { width : 95 / roomcolumns.length + '%' }
+                }))
+            ]
+        })))
     ]
 })
 
