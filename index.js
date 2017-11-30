fd70b35828699f7da05c7be434b9f0786eada8ee
aristov <vv.aristov@gmail.com>
2017-11-30 21:06:22 +0300

    IDRefAttrType

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import { p, main } from 'htmlmodule'
+import { main, p, span } from 'htmlmodule'
 import { button } from './lib/button'
 
 main({
     parentNode : document.body,
-    children : p(button({
-        pressed : 'false',
-        children : 'Test button'
-    }))
+    children : p([
+        button({
+            pressed : 'false',
+            errorMessage : 'errorspan',
+            children : 'Test button'
+        }),
+        span({
+            id : 'errorspan',
+            style : { margin : '10px' },
+            children : 'Error!'
+        })
+    ])
 })
