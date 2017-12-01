3d0cb5edd113795ca1aea1439b36f868ac4c19f6
aristov <vv.aristov@gmail.com>
2017-12-01 18:56:07 +0300

    Button test

---
+++ docs/tests/button.test.js
@@ -0,0 +1,31 @@
+import { a, article, h1, section } from 'htmlmodule'
+import { button } from '../../lib/index'
+
+article({
+    parentNode : document.body,
+    children : [
+        h1(a('Button')),
+        section([
+            button('Simple')
+        ]),
+        section([
+            button({
+                pressed : 'true',
+                children : 'Pressed toggle button'
+            })
+        ]),
+        section([
+            button({
+                disabled : true,
+                children : 'Disabled'
+            })
+        ]),
+        section([
+            button({
+                pressed : 'true',
+                disabled : true,
+                children : 'Pressed disabled toggle button'
+            })
+        ])
+    ]
+})
