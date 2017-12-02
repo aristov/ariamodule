ba059ef55ce57a0ecfeaa86e62c330af7a29b832
aristov <vv.aristov@gmail.com>
2017-12-02 09:36:31 +0300

    Total refactoring

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-// import './tests/link.test'
-// import './tests/button.test'
-// import './tests/checkbox.test'
+import './tests/link.test'
+import './tests/button.test'
+import './tests/checkbox.test'
 import './tests/dialog.test'
