80b09a079fe391eac710d084ed1379bd1965c078
aristov <vv.aristov@gmail.com>
2017-12-03 01:49:31 +0300

    Listbox multiselectable and chackable

--- docs/tests/listbox.test.js
+++ docs/tests/listbox.test.js
@@ -14,6 +14,80 @@ article({
                     new Option({ value : '3', children : 'Third option' })
                 ]
             })
+        ]),
+        section([
+            new Listbox({
+                label : 'Multiselectable',
+                multiselectable : true,
+                children : [
+                    new Option({ value : '1', children : 'First option' }),
+                    new Option({ value : '2', children : 'Second option' }),
+                    new Option({ value : '3', children : 'Third option' })
+                ]
+            })
+        ]),
+        section([
+            new Listbox({
+                label : 'Disabled',
+                disabled : true,
+                children : [
+                    new Option({ value : '1', children : 'First option' }),
+                    new Option({ value : '2', children : 'Second option' }),
+                    new Option({ value : '3', children : 'Third option' })
+                ]
+            })
         ])
+    ]
+})
+
+article({
+    parentNode : document.body,
+    children : [
+        h1(a('Check listbox')),
+        section([
+            new Listbox({
+                label : 'Simple',
+                children : [
+                    new Option({
+                        value : '1',
+                        checked : 'false',
+                        children : 'First option'
+                    }),
+                    new Option({
+                        value : '2',
+                        checked : 'false',
+                        children : 'Second option'
+                    }),
+                    new Option({
+                        value : '3',
+                        checked : 'false',
+                        children : 'Third option'
+                    })
+                ]
+            })
+        ]),
+        section([
+            new Listbox({
+                label : 'Multiselectable',
+                multiselectable : true,
+                children : [
+                    new Option({
+                        value : '1',
+                        checked : 'false',
+                        children : 'First option'
+                    }),
+                    new Option({
+                        value : '2',
+                        checked : 'false',
+                        children : 'Second option'
+                    }),
+                    new Option({
+                        value : '3',
+                        checked : 'false',
+                        children : 'Third option'
+                    })
+                ]
+            })
+        ]),
     ]
 })
