df6aa12727d28260d5d09bb15e4545046c87ecb9
aristov <vv.aristov@gmail.com>
2018-02-19 13:51:18 +0300

    Slider: labels and styling, add to showcase

--- docs/tests/slider.test.js
+++ docs/tests/slider.test.js
@@ -1,4 +1,4 @@
-import { a, article, h1, section, Span } from 'htmlmodule'
+import { a, article, h1, label, section, Span } from 'htmlmodule'
 import { Slider } from '../../lib'
 
 article({
@@ -7,15 +7,18 @@ article({
     children : [
         h1(a('Slider')),
         section([
+            label({ id : 'slider1', children : 'Simple' }),
             new Span({
                 className : 'slidertrack',
-                children : new Slider
+                children : new Slider({ labelledBy : 'slider1' })
             })
         ]),
         section([
+            label({ id : 'slider2', children : 'Custom properties' }),
             new Span({
                 className : 'slidertrack',
                 children : new Slider({
+                    labelledBy : 'slider2',
                     valueMin : -10,
                     valueNow : -5,
                     valueMax : 10
@@ -23,11 +26,12 @@ article({
             })
         ]),
         section([
+            label({ id : 'slider3', children : 'Multi-thumb' }),
             new Span({
                 className : 'slidertrack',
                 children : [
-                    new Slider({ valueNow : 30, }),
-                    new Slider({ valueNow : 70, })
+                    new Slider({ labelledBy : 'slider3', valueNow : 30 }),
+                    new Slider({ labelledBy : 'slider3', valueNow : 70 })
                 ]
             })
         ])
