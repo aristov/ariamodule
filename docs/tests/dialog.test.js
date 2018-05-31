4280a3f8e329372aa766e015d69c997acc5ddbf0
aristov <vv.aristov@gmail.com>
2018-06-01 00:06:37 +0300

    Dialog, AlertDialog: reintroduce the `modal` property on simple dialogs, rethink implementation Button.activate: toggle the `expanded` state + add support of dialogs usage by `hasPopup` and `controls` (wip)

--- docs/tests/dialog.test.js
+++ docs/tests/dialog.test.js
@@ -1,5 +1,5 @@
 import { a, article, h1, kbd, p, section } from 'htmlmodule'
-import { Button, Dialog, Heading, AlertDialog } from '../../lib'
+import { AlertDialog, Button, Dialog, Heading } from '../../lib'
 
 class CancelButton extends Button {
     activate() {
@@ -7,103 +7,140 @@ class CancelButton extends Button {
     }
 }
 
+let dialog, modalDialog
+
 article({
     parentNode : document.body,
     children : [
         h1(a('Dialog')),
         section([
             new Button({
-                onclick : ({ target }) => {
-                    const btn = Button.getRoleOf(target)
-                    const dlg = btn.controls[0]
-                    if(dlg) {
-                        dlg.remove()
-                    }
-                    else {
-                        new Dialog({
-                            previousSibling : target,
-                            trigger : target,
-                            classList : 'popup',
-                            children : [
-                                new Heading('Dialog'),
-                                p('This is a simple dialog.'),
-                                p(['It closes on the ', kbd('Escape'), ' key press or an outside click.']),
-                                new Button('Ok'), ' ',
-                                new CancelButton('Cancel')
-                            ]
-                        })
-                    }
-                },
-                expanded : false,
-                children : 'Simple dialog'
+                controls : dialog = new Dialog({
+                    hidden : true,
+                    children : [
+                        new Heading('Dialog'),
+                        p('This is a simple dialog.'),
+                        p(['It closes on the ', kbd('Escape'), ' key press or an outside click.']),
+                        new Button('Ok'),
+                        new CancelButton('Cancel')
+                    ]
+                }),
+                children : 'Show dialog'
+            }),
+        ]),
+        dialog,
+        section([
+            new Button({
+                controls : new Dialog([
+                    new Heading('Appended dialog'),
+                    p('This is a simple dialog.'),
+                    p(['It closes on the ', kbd('Escape'), ' key press or an outside click.']),
+                    new Button('Ok'),
+                    new CancelButton('Cancel')
+                ]),
+                children : 'Append dialog'
             })
         ]),
         section([
             new Button({
                 onclick : ({ target }) => {
                     const btn = Button.getRoleOf(target)
-                    const dlg = btn.controls[0]
-                    if(dlg) {
-                        dlg.remove()
-                    }
-                    else {
-                        new Dialog({
+                    if(!btn.controls.length) {
+                        btn.controls = new Dialog({
                             previousSibling : target,
-                            trigger : target,
+                            children : [
+                                new Heading('Created dialog'),
+                                p('This is a simple dialog.'),
+                                p(['It closes on the ', kbd('Escape'), ' key press or an outside click.']),
+                                new Button('Ok'),
+                                new CancelButton('Cancel')
+                            ]
+                        })
+                    }
+                },
+                hasPopup : 'dialog',
+                children : 'Create dialog'
+            })
+        ]),
+        section([
+            new Button({
+                onclick : ({ target }) => {
+                    const btn = Button.getRoleOf(target)
+                    if(!btn.controls.length) {
+                        btn.controls = new Dialog({
+                            previousSibling : target,
                             oncancel : event => event.preventDefault(),
                             // oncancel : event => false, // todo
-                            classList : 'popup',
                             children : [
                                 new Heading('Assertive dialog'),
                                 p('This dialog is assertive.'),
                                 p(['It doesn\'t close on the ', kbd('Escape'), ' key press or an outside click.']),
-                                new Button('Ok'), ' ',
+                                new Button('Ok'),
                                 new CancelButton('Cancel')
                             ]
                         })
                     }
                 },
-                expanded : false,
-                children : 'Assertive dialog'
+                hasPopup : 'dialog',
+                children : 'Create assertive dialog'
+            })
+        ]),
+        section([
+            new Button({
+                controls : new Dialog({
+                    modal : true,
+                    hidden : true,
+                    children : [
+                        new Heading('Appended modal dialog'),
+                        p('This is a simple dialog.'),
+                        p(['It closes on the ', kbd('Escape'), ' key press or an outside click.']),
+                        new Button('Ok'),
+                        new CancelButton('Cancel')
+                    ]
+                }),
+                children : 'Append modal dialog'
             })
         ]),
         section([
             new Button({
                 onclick : ({ target }) => {
-                    new AlertDialog({
-                        trigger : target,
-                        classList : 'popup',
-                        children : [
-                            new Heading('Alert dialog'),
-                            p('This is a simple alert dialog.'),
-                            p(['It is modal, but it closes on the ', kbd('Escape'), ' key press or an outside click.']),
-                            new Button('Ok'), ' ',
-                            new CancelButton('Cancel')
-                        ]
-                    })
+                    const btn = Button.getRoleOf(target)
+                    if(!btn.controls.length) {
+                        btn.controls = new Dialog({
+                            modal : true,
+                            children : [
+                                new Heading('Created modal dialog'),
+                                p('This is a simple modal dialog.'),
+                                p(['It is modal, but it closes on the ', kbd('Escape'), ' key press or an outside click.']),
+                                new Button('Ok'),
+                                new CancelButton('Cancel')
+                            ]
+                        })
+                    }
                 },
-                expanded : false,
-                children : 'Alert dialog'
+                hasPopup : 'dialog',
+                children : 'Create modal dialog'
             })
         ]),
         section([
             new Button({
                 onclick : ({ target }) => {
-                    new AlertDialog({
-                        trigger : target,
-                        oncancel : event => event.preventDefault(),
-                        classList : 'popup',
-                        children : [
-                            new Heading('Assertive alert dialog'),
-                            p('This modal dialog is assertive.'),
-                            p(['It doesn\'t close on the ', kbd('Escape'), ' key press or an outside click.']),
-                            new Button('Ok'), ' ',
-                            new CancelButton('Cancel')
-                        ]
-                    })
+                    const btn = Button.getRoleOf(target)
+                    if(!btn.controls.length) {
+                        btn.controls = new Dialog({
+                            oncancel : event => event.preventDefault(),
+                            modal : true,
+                            children : [
+                                new Heading('Assertive modal dialog'),
+                                p('This modal dialog is assertive.'),
+                                p(['It doesn\'t close on the ', kbd('Escape'), ' key press or an outside click.']),
+                                new Button('Ok'),
+                                new CancelButton('Cancel')
+                            ]
+                        })
+                    }
                 },
-                expanded : false,
-                children : 'Assertive alert dialog'
+                children : 'Create assertive modal dialog'
             })
         ])
     ]
