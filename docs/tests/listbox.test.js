c856eeb42c22b30c78bb430efaa6bc6104bcde33
aristov <vv.aristov@gmail.com>
2018-06-12 09:16:07 +0300

    Listbox + Option (wip)

--- docs/tests/listbox.test.js
+++ docs/tests/listbox.test.js
@@ -4,56 +4,7 @@ import { ListBox, Option } from '../../lib'
 article({
     parentNode : document.body,
     children : [
-        h1(a('List box')),
-        section([
-            new ListBox({
-                label : label('Simple'),
-                name : 'listbox-simple',
-                options : [
-                    new Option({
-                        value : '1',
-                        children : 'First option'
-                    }),
-                    new Option({
-                        value : '2',
-                        children : 'Second option'
-                    }),
-                    new Option({
-                        value : '3',
-                        children : 'Third option'
-                    }),
-                    new Option({
-                        value : '4',
-                        children : 'Fourth option'
-                    }),
-                    new Option({
-                        value : '5',
-                        children : 'Fifth option'
-                    })
-                ]
-            })
-        ]),
-        section([
-            new ListBox({
-                label : label('Selected'),
-                name : 'listbox-selected',
-                options : [
-                    new Option({
-                        value : '1',
-                        children : 'First option'
-                    }),
-                    new Option({
-                        value : '2',
-                        selected : true,
-                        children : 'Second option'
-                    }),
-                    new Option({
-                        value : '3',
-                        children : 'Third option'
-                    })
-                ]
-            })
-        ]),
+        h1(a('Multi list box')),
         section([
             new ListBox({
                 label : label('Multiselectable'),
@@ -95,6 +46,102 @@ article({
                     new Option({
                         value : '9',
                         children : 'Ninth option'
+                    })
+                ]
+            })
+        ]),
+        section([
+            new ListBox({
+                label : label('Selected'),
+                name : 'listbox-multiselectable',
+                multiselectable : true,
+                options : [
+                    new Option({
+                        value : '1',
+                        children : 'First option'
+                    }),
+                    new Option({
+                        value : '2',
+                        children : 'Second option'
+                    }),
+                    new Option({
+                        value : '3',
+                        selected : true,
+                        children : 'Third option'
+                    }),
+                    new Option({
+                        value : '4',
+                        selected : true,
+                        children : 'Fourth option'
+                    }),
+                    new Option({
+                        value : '5',
+                        selected : true,
+                        children : 'Fifth option'
+                    }),
+                    new Option({
+                        value : '6',
+                        children : 'Sixth option'
+                    }),
+                    new Option({
+                        value : '7',
+                        children : 'Seventh option'
+                    }),
+                    new Option({
+                        value : '8',
+                        children : 'Eighth option'
+                    }),
+                    new Option({
+                        value : '9',
+                        children : 'Ninth option'
+                    })
+                ]
+            })
+        ]),
+    ]
+})
+
+article({
+    parentNode : document.body,
+    children : [
+        h1(a('List box')),
+        section([
+            new ListBox({
+                label : label('Simple'),
+                name : 'listbox-simple',
+                options : [
+                    new Option({
+                        value : '1',
+                        children : 'First option'
+                    }),
+                    new Option({
+                        value : '2',
+                        children : 'Second option'
+                    }),
+                    new Option({
+                        value : '3',
+                        children : 'Third option'
+                    })
+                ]
+            })
+        ]),
+        section([
+            new ListBox({
+                label : label('Selected'),
+                name : 'listbox-selected',
+                options : [
+                    new Option({
+                        value : '1',
+                        children : 'First option'
+                    }),
+                    new Option({
+                        value : '2',
+                        selected : true,
+                        children : 'Second option'
+                    }),
+                    new Option({
+                        value : '3',
+                        children : 'Third option'
                     })
                 ]
             })
