8d7dcfed55b463a42fd1e7d61e3a098d17635d82
aristov <vv.aristov@gmail.com>
2018-08-19 18:33:51 +0300

    Edit tests

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 const buildPath = path.join(__dirname, 'docs/build')
 
 module.exports = {
-    entry: './docs/tests/index.test.js',
+    entry: './docs/showcase/index.test.js',
     output: {
         path : buildPath,
         filename: 'build.index.js'
