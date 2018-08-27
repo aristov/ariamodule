8a87175496d9642bfa8d1b13a0752d7f211c6a52
aristov <vv.aristov@gmail.com>
2018-08-27 22:37:45 +0300

    RadioGroup.label: prepend visible label if element passed

--- docs/showcase/radiogroup.test.js
+++ docs/showcase/radiogroup.test.js
@@ -6,82 +6,90 @@ article({
     children : [
         h1(a('Radio group')),
         section([
-            new RadioGroup([
-                label('Simple'),
-                new Radio({
-                    value : '1',
-                    children : 'First'
-                }),
-                new Radio({
-                    value : '2',
-                    children : 'Second'
-                }),
-                new Radio({
-                    value : '3',
-                    children : 'Third'
-                })
-            ])
+            new RadioGroup({
+                label : label('Simple'),
+                children : [
+                    new Radio({
+                        value : '1',
+                        children : 'First'
+                    }),
+                    new Radio({
+                        value : '2',
+                        children : 'Second'
+                    }),
+                    new Radio({
+                        value : '3',
+                        children : 'Third'
+                    })
+                ]
+            })
         ]),
         section([
-            new RadioGroup([
-                label('Checked'),
-                new Radio({
-                    value : '1',
-                    children : 'First'
-                }),
-                new Radio({
-                    value : '2',
-                    checked : true,
-                    children : 'Second'
-                }),
-                new Radio({
-                    value : '3',
-                    children : 'Third'
-                })
-            ])
+            new RadioGroup({
+                label : label('Checked'),
+                children : [
+                    new Radio({
+                        value : '1',
+                        children : 'First'
+                    }),
+                    new Radio({
+                        value : '2',
+                        checked : true,
+                        children : 'Second'
+                    }),
+                    new Radio({
+                        value : '3',
+                        children : 'Third'
+                    })
+                ]
+            })
         ]),
         section([
-            new RadioGroup([
-                label('Single disabled'),
-                new Radio({
-                    value : '1',
-                    disabled : true,
-                    children : 'First'
-                }),
-                new Radio({
-                    value : '2',
-                    children : 'Second'
-                }),
-                new Radio({
-                    value : '3',
-                    children : 'Third'
-                })
-            ])
+            new RadioGroup({
+                label : label('Single disabled'),
+                children : [
+                    new Radio({
+                        value : '1',
+                        disabled : true,
+                        children : 'First'
+                    }),
+                    new Radio({
+                        value : '2',
+                        children : 'Second'
+                    }),
+                    new Radio({
+                        value : '3',
+                        children : 'Third'
+                    })
+                ]
+            })
         ]),
         section([
-            new RadioGroup([
-                label('Checked and single disabled'),
-                new Radio({
-                    value : '1',
-                    checked : true,
-                    children : 'First'
-                }),
-                new Radio({
-                    value : '2',
-                    disabled : true,
-                    children : 'Second'
-                }),
-                new Radio({
-                    value : '3',
-                    children : 'Third'
-                })
-            ])
+            new RadioGroup({
+                label : label('Checked and single disabled'),
+                children : [
+                    new Radio({
+                        value : '1',
+                        checked : true,
+                        children : 'First'
+                    }),
+                    new Radio({
+                        value : '2',
+                        disabled : true,
+                        children : 'Second'
+                    }),
+                    new Radio({
+                        value : '3',
+                        children : 'Third'
+                    })
+                ]
+            })
         ]),
         section([
             new RadioGroup({
                 disabled : true,
+                label : label('Group disabled'),
                 children : [
-                    label('Group disabled'),
                     new Radio({
                         value : '1',
                         children : 'First'
