f31b04a652e6d5a5ebc2ff56c5cfb378c788ca5e
aristov <vv.aristov@gmail.com>
2018-09-05 03:32:04 +0300

    MenuBar: support vertical orientation

--- docs/showcase/menu.test.js
+++ docs/showcase/menu.test.js
@@ -114,81 +114,98 @@ article({
     children : [
         h1(a('Complex menu bar')),
         section([
-            new MenuBar([
-                new MenuItem({
-                    children : 'Mixed menu',
-                    controls : new Menu([
-                        new MenuItemCheckBox('Check item'),
-                        new Separator,
-                        new MenuItem({
-                            children : 'Nested submenu',
-                            controls : new Menu([
-                                new MenuItemRadio('First radio item'),
-                                new MenuItemRadio('Second radio item'),
-                                new MenuItemRadio('Third radio item')
-                            ])
-                        }),
-                        new MenuItem({
-                            children : 'Follow link',
-                            ownerElement : a({
-                                href : '//www.w3.org/TR/wai-aria/#menu',
-                                target : '_blank'
-                            })
-                        }),
-                        new Separator,
-                        new Group([
-                            new MenuItemRadio('First radio'),
-                            new MenuItemRadio('Second radio')
-                        ]),
-                        new Separator,
-                        new MenuItem({
-                            disabled : true,
-                            children : 'Disabled item'
-                        }),
-                        new MenuItem({
-                            children : 'Open dialog...',
-                            hasPopup : 'dialog',
-                            onclick : openDialog
-                        })
-                    ])
-                }),
-                new MenuItem({
-                    children : 'Check menu',
-                    controls : new Menu([
-                        new MenuItemCheckBox('First check item'),
-                        new MenuItemCheckBox({
-                            checked : true,
-                            children : 'Second check item'
-                        }),
-                        new MenuItemCheckBox('Third check item')
-                    ])
-                }),
-                new MenuItem({
-                    children : 'Radio menu groups',
-                    controls : new Menu([
-                        new Group([
-                            new MenuItemRadio({
-                                disabled : true,
-                                children : 'First group - radio one'
-                            }),
-                            new MenuItemRadio('First group - radio two'),
-                            new MenuItemRadio('First group - radio three')
-                        ]),
-                        new Separator,
-                        new Group([
-                            new MenuItemRadio({
-                                checked : true,
-                                children : 'Second group - radio one'
-                            }),
-                            new MenuItemRadio('Second group - radio two'),
-                            new MenuItemRadio({
-                                disabled : true,
-                                children : 'Second group - radio three'
-                            }),
-                        ])
-                    ])
-                })
-            ])
+            complexMenuBar()
         ])
     ]
 })
+
+article({
+    parentNode : document.body,
+    children : [
+        h1(a('Vertical menu bar')),
+        section([
+            complexMenuBar('vertical')
+        ])
+    ]
+})
+
+function complexMenuBar(orientation) {
+    return new MenuBar({
+        orientation,
+        children : [
+            new MenuItem({
+                children : 'Mixed menu',
+                controls : new Menu([
+                    new MenuItemCheckBox('Check item'),
+                    new Separator,
+                    new MenuItem({
+                        children : 'Nested submenu',
+                        controls : new Menu([
+                            new MenuItemRadio('First radio item'),
+                            new MenuItemRadio('Second radio item'),
+                            new MenuItemRadio('Third radio item')
+                        ])
+                    }),
+                    new MenuItem({
+                        children : 'Follow link',
+                        ownerElement : a({
+                            href : '//www.w3.org/TR/wai-aria/#menu',
+                            target : '_blank'
+                        })
+                    }),
+                    new Separator,
+                    new Group([
+                        new MenuItemRadio('First radio'),
+                        new MenuItemRadio('Second radio')
+                    ]),
+                    new Separator,
+                    new MenuItem({
+                        disabled : true,
+                        children : 'Disabled item'
+                    }),
+                    new MenuItem({
+                        children : 'Open dialog...',
+                        hasPopup : 'dialog',
+                        onclick : openDialog
+                    })
+                ])
+            }),
+            new MenuItem({
+                children : 'Check menu',
+                controls : new Menu([
+                    new MenuItemCheckBox('First check item'),
+                    new MenuItemCheckBox({
+                        checked : true,
+                        children : 'Second check item'
+                    }),
+                    new MenuItemCheckBox('Third check item')
+                ])
+            }),
+            new MenuItem({
+                children : 'Radio menu groups',
+                controls : new Menu([
+                    new Group([
+                        new MenuItemRadio({
+                            disabled : true,
+                            children : 'First group - radio one'
+                        }),
+                        new MenuItemRadio('First group - radio two'),
+                        new MenuItemRadio('First group - radio three')
+                    ]),
+                    new Separator,
+                    new Group([
+                        new MenuItemRadio({
+                            checked : true,
+                            children : 'Second group - radio one'
+                        }),
+                        new MenuItemRadio('Second group - radio two'),
+                        new MenuItemRadio({
+                            disabled : true,
+                            children : 'Second group - radio three'
+                        })
+                    ])
+                ])
+            })
+        ]
+    })
+}
