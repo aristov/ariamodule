685dda45540b8e80474ee7c29cd171f25fa4f42d
aristov <vv.aristov@gmail.com>
2018-09-28 16:21:53 +0300

    Slider: Rethink implementation (wip)

--- docs/showcase/Slider.test.js
+++ docs/showcase/Slider.test.js
@@ -1,38 +1,57 @@
-import { a, article, h1, label, section, Span } from 'htmlmodule'
+import { a, article, h1, label, section } from 'htmlmodule'
 import { Slider } from './ariamodule'
-
-class SliderTrack extends Span {}
 
 article({
     parentNode : document.body,
-    id : 'example-slider',
     children : [
         h1(a('Slider')),
         section([
-            label({ id : 'slider1', children : 'Simple' }),
-            new SliderTrack(new Slider({ labelledBy : 'slider1' }))
+            new Slider({ label : label('Simple') })
         ]),
         section([
-            label({ id : 'slider2', children : 'Custom properties' }),
-            new SliderTrack(new Slider({
-                labelledBy : 'slider2',
-                valueMin : -10,
-                valueNow : -5,
-                valueMax : 10
-            }))
+            new Slider({
+                label : label('Custom properties'),
+                valueMin : -2,
+                valueMax : 2,
+                valueNow : -1
+            })
         ]),
         section([
-            label({ id : 'slider3', children : 'Multi-thumb' }),
-            new SliderTrack([
-                new Slider({
-                    labelledBy : 'slider3',
-                    valueNow : 30
-                }),
-                new Slider({
-                    labelledBy : 'slider3',
-                    valueNow : 70
-                })
-            ])
+            new Slider({
+                label : label('Multi-thumb'),
+                valueNow : [20, 50, 80]
+            })
+        ]),
+        section([
+            new Slider({
+                label : label('Read only'),
+                readOnly : true
+            })
+        ]),
+        section([
+            new Slider({
+                label : label('Disabled'),
+                disabled : true
+            })
+        ]),
+        section([
+            h1('Vertical slider'),
+            new Slider({
+                label : label('Simple'),
+                orientation : 'vertical'
+            }),
+            new Slider({
+                label : label('Custom'),
+                orientation : 'vertical',
+                valueMin : -2,
+                valueMax : 2,
+                valueNow : -1
+            }),
+            new Slider({
+                label : label('Multi-thumb'),
+                orientation : 'vertical',
+                valueNow : [30, 70]
+            })
         ])
     ]
 })
