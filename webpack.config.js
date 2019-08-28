19917969714cbf9272259973f864d2eb311c8f6c
aristov <vv.aristov@gmail.com>
2019-08-28 20:04:28 +0300

    Webpack: configure style related loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,38 @@
 const path = require('path')
 const buildPath = path.join(__dirname, 'docs/build')
 
-module.exports = {
-    entry: './docs/showcase/index.test.js',
-    output: {
-        path : buildPath,
-        filename: 'build.index.js'
-    },
-    module : {
-        rules : process.env.NODE_ENV === 'production'? [
+const CSS_EXT_RE = /\.css$/
+
+const rules = [
+    {
+        test : CSS_EXT_RE,
+        use : [
+            'style-loader',
             {
-                test : /\.js$/,
-                exclude: /(node_modules\/babel-polyfill|node_modules\/dom4|node_modules\/shim-keyboard-event-key)/,
-                use : { loader : 'babel-loader' }
-            }
-        ] : []
+                loader : 'css-loader',
+                options : {
+                    importLoaders : 1,
+                    url : false
+                }
+            },
+            'postcss-loader'
+        ]
     }
+]
+
+if(process.env.NODE_ENV === 'production') {
+    rules.push({
+        test : /\.js$/,
+        exclude : /(node_modules\/babel-polyfill|node_modules\/dom4|node_modules\/shim-keyboard-event-key)/,
+        use : { loader : 'babel-loader' }
+    })
+}
+
+module.exports = {
+    entry : './docs/showcase/index.test.js',
+    output : {
+        path : buildPath,
+        filename : 'build.index.js'
+    },
+    module : { rules }
 }
