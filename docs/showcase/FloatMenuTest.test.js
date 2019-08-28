b6c898647246aeee38eeb2a07e6254ba26cf30c6
aristov <vv.aristov@gmail.com>
2019-08-28 19:40:10 +0300

    Menu.setPosition: implement method to set proper fixed position of menu on screen, add tests

---
+++ docs/showcase/FloatMenuTest.test.js
@@ -0,0 +1,28 @@
+import { Button, Group, Menu, MenuItem } from './ariamodule'
+
+class FloatMenuTest extends Group {
+}
+
+function button() {
+    return new Button({
+        controls : new Menu([
+            new MenuItem('First item'),
+            new MenuItem('Second item'),
+            new MenuItem('Third item'),
+            new MenuItem('Fourth item'),
+            new MenuItem('Fifth item'),
+            new MenuItem('Sixth item'),
+        ]),
+        children : 'Menu'
+    })
+}
+
+new FloatMenuTest({
+    parentNode : document.body,
+    children : [
+        button(),
+        button(),
+        button(),
+        button()
+    ]
+})
