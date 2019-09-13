b97f5c2a5bc29827289bf6b5d93808bae0c75c9a
aristov <vv.aristov@gmail.com>
2019-09-13 22:17:29 +0300

    Edit example

--- docs/example.js
+++ docs/example.js
@@ -1,14 +1,64 @@
-import { Main, Heading, Paragraph, Form, TextBox, Button } from '../lib'
+import { ElementAssembler } from 'dommodule'
+import { Role, Widget, Main, Heading, Paragraph, Form, ARIATextBox, ARIAButton } from '../lib'
+
+Role.debug()
+
+class Span extends ElementAssembler {
+    static get namespace() {
+        return 'http://www.w3.org/1999/xhtml'
+    }
+
+    static get localName() {
+        return super.localName.toLowerCase()
+    }
+}
+
+class Div extends Span {}
+
+class TextBox extends ARIATextBox {
+    create(init) {
+        super.create(init)
+        this.node.contentEditable = true
+        this.on('keydown', event => {
+            if(event.key === 'Enter') {
+                event.preventDefault()
+                this.form.emit('submit')
+            }
+        })
+    }
+}
+
+class Button extends ARIAButton {
+    create(init) {
+        super.create(init)
+        this.node.tabIndex = 0,
+            this.on('click', event => this.form.emit('submit'))
+        this.on('keydown', event => {
+            [' ', 'Enter'].includes(event.key) && this.node.click()
+        })
+    }
+}
+
+Role.elementAssembler = Div
+Widget.elementAssembler = Span
+
+let label, textBox
 
 new Main({
     parentNode : document.body,
     children : [
         new Heading('Hello world!'),
         new Paragraph('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-        new Form([
-            new TextBox('Name'),
-            ' ',
-            new Button('Confirm!')
-        ])
+        new Form({
+            onsubmit : event => location.search = '?name=' + textBox.value,
+            children : [
+                label = new Span('Name'),
+                textBox = new TextBox({
+                    labelledBy : label,
+                    children : (new URL(location)).searchParams.get('name') || ' '
+                }),
+                new Button('Confirm!')
+            ]
+        })
     ]
 })
