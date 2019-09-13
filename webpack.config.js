958bccf58b5af148e8a4e96803d15efbc85d63b5
aristov <vv.aristov@gmail.com>
2019-09-13 13:49:55 +0300

    NPM, Webpack: configure example and production builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,51 @@
 'use strict'
 
 const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const rules = [
-    /*{
-        test : /\.css$/,
-        use : [
-            'style-loader',
-            {
-                loader : 'css-loader',
-                options : {
-                    importLoaders : 1,
-                    url : false
-                }
-            },
-            'postcss-loader'
-        ]
-    }*/
-]
-
-if(process.env.NODE_ENV === 'production') {
-    rules.push({
-        test : /\.js$/,
-        exclude : /(node_modules\/babel-polyfill|node_modules\/dom4|node_modules\/shim-keyboard-event-key)/,
-        use : { loader : 'babel-loader' }
-    })
-}
-
-module.exports = {
-    mode : 'none',
-    entry : './lib/index.js',
-    output : {
-        path : path.join(__dirname, 'dist'),
-        filename : 'dist.ariamodule.js'
+module.exports = [
+    {
+        mode : 'none',
+        entry : './lib/index.js',
+        output : {
+            path : path.join(__dirname, 'dist'),
+            filename : 'dist.ariamodule.js'
+        }
     },
-    module : { rules }
-}
+    {
+        mode : 'none',
+        entry : './lib/index.js',
+        output : {
+            path : path.join(__dirname, 'dist'),
+            filename : 'dist.ariamodule.min.js'
+        },
+        module : {
+            rules : [
+                {
+                    test : /\.js$/,
+                    exclude : /(node_modules\/babel-polyfill|node_modules\/dom4|node_modules\/shim-keyboard-event-key)/,
+                    use : { loader : 'babel-loader' }
+                }
+            ]
+        },
+        plugins : [
+            new UglifyJsPlugin({
+                uglifyOptions : {
+                    keep_fnames : true,
+                    keep_classnames : true,
+                    output : {
+                        comments : false
+                    }
+                }
+            })
+        ]
+    },
+    {
+        mode : 'none',
+        entry : './docs/example.js',
+        output : {
+            path : path.join(__dirname, 'docs/build'),
+            filename : 'build.example.js'
+        }
+    }
+]
