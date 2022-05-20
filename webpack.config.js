683b475411ab31a4bde5854fbb5a658978fd0f2a
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-20 20:46:26 +0300

    Copying stuff from uimodule

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,44 @@
-'use strict'
+const TerserPlugin = require('terser-webpack-plugin')
+const JS_EXT = process.env.NODE_ENV === 'production' ? '.min.js' : '.js'
 
-const path = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+module.exports = {
+  mode : 'none',
+  entry : './index.js',
+  output : {
+    filename : 'ariamodule' + JS_EXT,
+    library : {
+      name : 'ariamodule',
+      type : 'umd',
+    },
+    globalObject : 'this',
+  },
+}
 
-module.exports = [
-    {
-        mode : 'none',
-        entry : './lib/index.js',
-        output : {
-            path : path.join(__dirname, 'dist'),
-            filename : 'dist.ariamodule.js'
-        }
-    },
-    {
-        mode : 'none',
-        entry : './lib/index.js',
-        output : {
-            path : path.join(__dirname, 'dist'),
-            filename : 'dist.ariamodule.min.js'
+if(process.env.NODE_ENV === 'production') {
+  Object.assign(module.exports, {
+    module : {
+      rules : [
+        {
+          test : /\.js$/,
+          use : {
+            loader : 'babel-loader',
+            options : {
+              presets : ['@babel/preset-env'],
+              plugins : ['@babel/plugin-transform-runtime'],
+            },
+          },
         },
-        module : {
-            rules : [
-                {
-                    test : /\.js$/,
-                    exclude : /(node_modules\/babel-polyfill|node_modules\/dom4|node_modules\/shim-keyboard-event-key)/,
-                    use : { loader : 'babel-loader' }
-                }
-            ]
-        },
-        plugins : [
-            new UglifyJsPlugin({
-                uglifyOptions : {
-                    keep_fnames : true,
-                    keep_classnames : true,
-                    output : {
-                        comments : false
-                    }
-                }
-            })
-        ]
+      ],
     },
-    {
-        mode : 'none',
-        entry : './docs/example.js',
-        output : {
-            path : path.join(__dirname, 'docs/build'),
-            filename : 'build.example.js'
-        }
-    }
-]
+    optimization : {
+      minimize : true,
+      minimizer : [
+        new TerserPlugin({
+          terserOptions : {
+            keep_fnames : true,
+          },
+        }),
+      ],
+    },
+  })
+}
