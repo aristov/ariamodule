0d2419e48ed246ca1f75bc61935de0191c296eff
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-21 15:01:57 +0300

    Clean up

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/**
+ * @module ariamodule
+ * @author Vyacheslav Aristov <vv.aristov@gmail.com>
+ * @license MIT
+ * @see https://www.w3.org/TR/wai-aria
+ */
 module.exports = require('./lib/ariamodule.js')
