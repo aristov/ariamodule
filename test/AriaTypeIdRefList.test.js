a53239eb01bc697fcbcf12a096141ccb3d3d55b4
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-22 09:07:42 +0300

    AriaTypeIdRefList: adding tests

---
+++ test/AriaTypeIdRefList.test.js
@@ -0,0 +1,45 @@
+const test = require('ava')
+const { RoleButton } = require('..')
+
+test('id1', t => {
+  const instance = RoleButton.render({
+    labelledBy : 'id1'
+  })
+  t.deepEqual(instance.labelledBy, ['id1'])
+  t.is(instance.toString(), '<div aria-labelledby="id1" role="Button"></div>')
+})
+
+test('[]', t => {
+  const instance = RoleButton.render({
+    labelledBy : []
+  })
+  t.deepEqual(instance.labelledBy, [])
+  t.is(instance.toString(), '<div role="Button"></div>')
+})
+
+test('[id1, id2, id3]', t => {
+  const instance = RoleButton.render({
+    labelledBy : ['id1', 'id2', 'id3']
+  })
+  t.deepEqual(instance.labelledBy, ['id1', 'id2', 'id3'])
+  t.is(instance.toString(), '<div aria-labelledby="id1 id2 id3" role="Button"></div>')
+})
+
+test('[id1, null, id3]', t => {
+  const instance = RoleButton.render({
+    labelledBy : ['id1', null, 'id3']
+  })
+  t.deepEqual(instance.labelledBy, ['id1', 'id3'])
+  t.is(instance.toString(), '<div aria-labelledby="id1 id3" role="Button"></div>')
+})
+
+test('null', t => {
+  const instance = RoleButton.render({
+    labelledBy : null
+  })
+  t.deepEqual(instance.labelledBy, [])
+  t.is(instance.toString(), '<div role="Button"></div>')
+
+  instance.node.setAttribute('aria-labelledby', '')
+  t.deepEqual(instance.labelledBy, [])
+})
