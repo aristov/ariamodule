acf1b08b4beddf2eb7619f4dded6b465161d343a
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-22 07:51:06 +0300

    AriaTypeInteger: adding tests

--- test/AriaTypeBoolean.test.js
+++ test/AriaTypeBoolean.test.js
@@ -8,14 +8,19 @@ test('true', t => {
   })
   t.is(instance.disabled, true)
   t.is(instance.toString(), '<div aria-disabled="true" role="Button">OK</div>')
+
   instance.disabled = 'true'
   t.is(instance.disabled, true)
+
   instance.disabled = 'foo'
   t.is(instance.disabled, true)
+
   instance.disabled = 1
   t.is(instance.disabled, true)
+
   instance.disabled = 42
   t.is(instance.disabled, true)
+
   instance.node.setAttribute('aria-disabled', 'foo')
   t.is(instance.disabled, true)
 })
@@ -27,18 +32,25 @@ test('false', t => {
   })
   t.is(instance.disabled, false)
   t.is(instance.toString(), '<div role="Button">OK</div>')
+
   instance.disabled = 'false'
   t.is(instance.disabled, false)
+
   instance.disabled = ''
   t.is(instance.disabled, false)
+
   instance.disabled = null
   t.is(instance.disabled, false)
+
   instance.disabled = undefined
   t.is(instance.disabled, false)
+
   instance.disabled = 0
   t.is(instance.disabled, false)
+
   instance.node.setAttribute('aria-disabled', 'false')
   t.is(instance.disabled, false)
+
   instance.node.setAttribute('aria-disabled', '')
   t.is(instance.disabled, false)
 })
---
+++ test/AriaTypeInteger.test.js
@@ -0,0 +1,170 @@
+const test = require('ava')
+const { RoleArticle } = require('..')
+
+test('0', t => {
+  const instance = RoleArticle.render({
+    text : 'Lorem ipsum',
+    posInSet : .0,
+  })
+  t.is(instance.posInSet, 0)
+  t.is(instance.toString(), '<div aria-posinset="0" role="Article">Lorem ipsum</div>')
+
+  instance.posInSet = .0e-1
+  t.is(instance.posInSet, 0)
+
+  instance.posInSet = ''
+  t.is(instance.posInSet, 0)
+
+  instance.posInSet = '.0'
+  t.is(instance.posInSet, 0)
+
+  instance.posInSet = '.0e-1'
+  t.is(instance.posInSet, 0)
+
+  instance.posInSet = []
+  t.is(instance.posInSet, 0)
+
+  instance.posInSet = [.0]
+  t.is(instance.posInSet, 0)
+
+  instance.posInSet = ['.0e-1']
+  t.is(instance.posInSet, 0)
+
+  instance.posInSet = false
+  t.is(instance.posInSet, 0)
+
+  instance.node.setAttribute('aria-posinset', '.0')
+  t.is(instance.posInSet, 0)
+
+  instance.node.setAttribute('aria-posinset', '.0e-1')
+  t.is(instance.posInSet, 0)
+
+  instance.node.setAttribute('aria-posinset', '')
+  t.is(instance.posInSet, 0)
+})
+
+test('1', t => {
+  const instance = RoleArticle.render({
+    text : 'Lorem ipsum',
+    posInSet : 1.1,
+  })
+  t.is(instance.posInSet, 1)
+  t.is(instance.toString(), '<div aria-posinset="1" role="Article">Lorem ipsum</div>')
+
+  instance.posInSet = 11e-1
+  t.is(instance.posInSet, 1)
+
+  instance.posInSet = '1.1'
+  t.is(instance.posInSet, 1)
+
+  instance.posInSet = '1.1'
+  t.is(instance.posInSet, 1)
+
+  instance.posInSet = '11e-1'
+  t.is(instance.posInSet, 1)
+
+  instance.posInSet = [1.1]
+  t.is(instance.posInSet, 1)
+
+  instance.posInSet = ['11e-1']
+  t.is(instance.posInSet, 1)
+
+  instance.posInSet = true
+  t.is(instance.posInSet, 1)
+
+  instance.node.setAttribute('aria-posinset', '1.1')
+  t.is(instance.posInSet, 1)
+
+  instance.node.setAttribute('aria-posinset', '11e-1')
+  t.is(instance.posInSet, 1)
+})
+
+test('4', t => {
+  const instance = RoleArticle.render({
+    text : 'Lorem ipsum',
+    posInSet : 4.2,
+  })
+  t.is(instance.posInSet, 4)
+  t.is(instance.toString(), '<div aria-posinset="4" role="Article">Lorem ipsum</div>')
+
+  instance.posInSet = 42e-1
+  t.is(instance.posInSet, 4)
+
+  instance.posInSet = '4.2'
+  t.is(instance.posInSet, 4)
+
+  instance.posInSet = '42e-1'
+  t.is(instance.posInSet, 4)
+
+  instance.posInSet = [4.2]
+  t.is(instance.posInSet, 4)
+
+  instance.posInSet = ['4.2']
+  t.is(instance.posInSet, 4)
+
+  instance.node.setAttribute('aria-posinset', '4.2')
+  t.is(instance.posInSet, 4)
+
+  instance.node.setAttribute('aria-posinset', '42e-1')
+  t.is(instance.posInSet, 4)
+})
+
+test('NaN', t => {
+  const instance = RoleArticle.render({
+    text : 'Lorem ipsum',
+    posInSet : NaN,
+  })
+  t.is(instance.posInSet, NaN)
+  t.is(instance.toString(), '<div aria-posinset="NaN" role="Article">Lorem ipsum</div>')
+
+  instance.posInSet = 'NaN'
+  t.is(instance.posInSet, NaN)
+
+  instance.posInSet = 'xyz'
+  t.is(instance.posInSet, NaN)
+
+  instance.posInSet = {}
+  t.is(instance.posInSet, NaN)
+
+  instance.posInSet = [.0, 4.2, 1.1]
+  t.is(instance.posInSet, NaN)
+
+  instance.posInSet = function() {}
+  t.is(instance.posInSet, NaN)
+
+  instance.posInSet = undefined
+  t.is(instance.posInSet, NaN)
+
+  instance.node.setAttribute('aria-posinset', 'true')
+  t.is(instance.posInSet, NaN)
+
+  instance.node.setAttribute('aria-posinset', 'false')
+  t.is(instance.posInSet, NaN)
+
+  instance.node.setAttribute('aria-posinset', 'undefined')
+  t.is(instance.posInSet, NaN)
+
+  instance.node.setAttribute('aria-posinset', 'xyz')
+  t.is(instance.posInSet, NaN)
+})
+
+test('Infinity', t => {
+  const instance = RoleArticle.render({
+    text : 'Lorem ipsum',
+    posInSet : Infinity,
+  })
+  t.is(instance.posInSet, Infinity)
+  t.is(instance.toString(), '<div aria-posinset="Infinity" role="Article">Lorem ipsum</div>')
+
+  instance.posInSet = 'Infinity'
+  t.is(instance.posInSet, Infinity)
+})
+
+test('null', t => {
+  const instance = RoleArticle.render({
+    text : 'Lorem ipsum',
+    posInSet : null,
+  })
+  t.is(instance.posInSet, null)
+  t.is(instance.toString(), '<div role="Article">Lorem ipsum</div>')
+})
